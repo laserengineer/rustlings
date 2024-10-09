93cf965b62a9136d1b5d02179ec7071d3f9fe58e
Yang Yu <yy19@ualberta.ca>
2024-10-08 20:13:30 -0600

    Finish string exercise

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -9,6 +9,11 @@ struct Point {
 #[derive(Debug)]
 enum Message {
     // TODO: Define the different variants used below.
+    Resize { width: u64, height: u64 },
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit, // No payload required for Quit variant.
 }
 
 impl Message {
